04a658a42fc5a9b88948d1470a22b8f5bf9bed4b
YavuzGurdal <57366318+YavuzGurdal@users.noreply.github.com>
2021-12-06 11:31:25 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,26 +8,26 @@ export const aboutMeData = {
         //     h6Title: '',
         //     pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porrosed dolor facilis suscipit eum aliquid quia illo,dolorem repudiandae!'
         // },
-        {
-            divClassName: 'job job-1',
-            h3ClassName: '',
-            h3Title: '123 Webshop',
-            h6Title: 'Lorem, ipsum dolor.',
-            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
-        },
-        {
-            divClassName: 'job job-2',
-            h3ClassName: '',
-            h3Title: 'Designer',
-            h6Title: 'Lorem, ipsum dolor.',
-            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
-        },
+        //{
+        //    divClassName: 'job job-1',
+        //    h3ClassName: '',
+        //    h3Title: '123 Webshop',
+        //    h6Title: 'Lorem, ipsum dolor.',
+        //    pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+        //},
+        //{
+        //    divClassName: 'job job-2',
+        //    h3ClassName: '',
+        //    h3Title: 'Designer',
+        //    h6Title: 'Lorem, ipsum dolor.',
+        //    pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+        //},
         {
             divClassName: 'job job-3',
             h3ClassName: '',
             h3Title: 'react developer',
-            h6Title: 'Lorem, ipsum dolor.',
-            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+            h6Title: '',
+            pText: ''
         }
 
     ]
